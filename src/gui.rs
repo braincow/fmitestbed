70aeb1a25d400fc0063eb800d527a21254e5634e
Antti Peltonen <antti.peltonen@iki.fi>
2018-08-25 12:52:19 +0300

    Ugly implementation of basic functionality.
    Thx to @vitalyd at users.rust-lang.org for helping out with Rc, RefCell.

--- src/gui.rs
+++ src/gui.rs
@@ -7,7 +7,7 @@ use gtk::prelude::*;
 use gtk::{Application, ApplicationWindow, Builder, Image};
 
 use std::env::args;
-use std::collections::HashMap;
+use std::collections::BTreeMap;
 use std::rc::Rc;
 use std::cell::RefCell;
 
@@ -32,67 +32,90 @@ macro_rules! clone {
     );
 }
 
-fn build_ui(application: &gtk::Application) {
-    // added during compilation (macro)
-    let glade_src = include_str!("fmitestbed.glade");
-    let builder = Builder::new_from_string(glade_src);
-
-    let main_window: ApplicationWindow = builder.get_object("main_window").expect("Couldn't get main_window");
-    let img_dbz: Image = builder.get_object("img_dBZ").expect("Could'nt get img_dBZ");
-    // loaded during runtime
-    img_dbz.set_from_file("resources/dbz.png");
-
-    main_window.set_application(application);
-    main_window.connect_delete_event(clone!(main_window => move |_, _| {
-        main_window.destroy();
-        Inhibit(false)
-    }));
-
-    main_window.show_all();
-}
-
-struct Datapoints {
-    datapoints: HashMap<String, Datapoint>
-}
-impl Datapoints {
-    fn new() -> Datapoints {
-        // return empty hashmap
-        Datapoints { datapoints: HashMap::new() }
-    }
-
-    fn tick(&mut self) -> Continue {
-        // download fmi data
-        self.datapoints = parse_testbed();
-
-        // we could return gtk::Continue(false) to stop our refreshing data
-        gtk::Continue(true)
-    }
-}
-
 pub struct TestbedGui {
     application: Application,
-    datapoints: RefCell<Datapoints>
+    datapoints: Rc<RefCell<BTreeMap<String, Datapoint>>>,
+    datapoint_image: Rc<RefCell<Image>>,
+    datapoint_position: Rc<RefCell<usize>>
 }
 impl TestbedGui {
-    pub fn new() -> Rc<TestbedGui> {
+    pub fn new() -> TestbedGui {
         let application = gtk::Application::new("me.bcow.fmitestbed",
             gio::ApplicationFlags::empty())
             .expect("Initialization failed...");
 
+        let img_datapoint: Rc<RefCell<Image>> = Rc::new(RefCell::new(Image::new()));
+
+        let img_datapoint_clone = img_datapoint.clone();
         application.connect_startup(move |app| {
-            build_ui(app);
+            // added during compilation (macro)
+            let glade_src = include_str!("fmitestbed.glade");
+            let builder = Builder::new_from_string(glade_src);
+
+            let main_window: ApplicationWindow = builder.get_object("main_window").expect("Couldn't get main_window");
+            let img_dbz: Image = builder.get_object("img_dBZ").expect("Could'nt get img_dBZ");
+            *img_datapoint_clone.borrow_mut() = builder.get_object("img_datapoint").expect("Could'nt get img_datapoint");
+            // loaded during runtime
+            img_dbz.set_from_file("resources/dbz.png");
+
+            main_window.set_application(app);
+            main_window.connect_delete_event(clone!(main_window => move |_, _| {
+                main_window.destroy();
+                Inhibit(false)
+            }));
+
+            main_window.show_all();
         });
         application.connect_activate(|_| {});
 
-        let instance = Rc::new(TestbedGui { application: application, datapoints: RefCell::new(Datapoints::new()) });
-        // executes the update every five minutes
-        instance.datapoints.borrow().tick();
-        //gtk::timeout_add_seconds(5 * 60, instance.datapoints.borrow().tick);
-
-        instance
+        TestbedGui { application: application,
+            datapoints: Rc::new(RefCell::new(parse_testbed())),
+            datapoint_image: img_datapoint,
+            datapoint_position: Rc::new(RefCell::new(0)) }
     }
 
     pub fn run(&self) {
+        // create a reference cycle clone of the datapoints in our struct and refresh data
+        let datapoints_clone = self.datapoints.clone();
+        let datapoints_position_clone = self.datapoint_position.clone();
+        let tick = move || {
+            //println!("tick");
+            // refresh the FMI testbed data
+            *datapoints_clone.borrow_mut() = parse_testbed();
+            *datapoints_position_clone.borrow_mut() = 0;
+            gtk::Continue(true)
+        };
+        gtk::timeout_add_seconds(5 * 60, tick);
+
+        // create a new clone (again)
+        let datapoints_clone = self.datapoints.clone();
+        let datapoints_position_clone = self.datapoint_position.clone();
+        let img_datapoint_clone = self.datapoint_image.clone();
+        let tock = move || {
+            //println!("tock");
+            let map = &*datapoints_clone.borrow();
+
+            let mut position: usize = 0;
+            let old_position = *datapoints_position_clone.borrow_mut();
+            for (_key, val) in map {
+                if position > old_position {
+                    //println!("get pixbuf for {}", _key);
+                    let pixbuf = val.image_as_pixbuf();
+                    *&img_datapoint_clone.borrow_mut().set_from_pixbuf(&pixbuf);
+                    break;
+                }
+                position = position + 1;
+            }
+            //println!("{}", position);
+            if position == 14 {
+                // when finding the end roll back to begin
+                position = 0;
+            }
+            *datapoints_position_clone.borrow_mut() = position;
+            gtk::Continue(true)
+        };
+        gtk::timeout_add_seconds(1, tock);
+
         // run Gtk main loop
         self.application.run(&args().collect::<Vec<_>>());
     }
--- src/parser.rs
+++ src/parser.rs
@@ -1,9 +1,9 @@
-use std::collections::HashMap;
+use std::collections::BTreeMap;
 use datapoint::Datapoint;
 use reqwest::get;
 use regex::Regex;
 
-pub fn parse_testbed() -> HashMap<String, Datapoint> {
+pub fn parse_testbed() -> BTreeMap<String, Datapoint> {
     // fetch HTML source for later parsing
     let mut res = match get("http://testbed.fmi.fi/") {
         Ok(res) => res,
@@ -17,10 +17,10 @@ pub fn parse_testbed() -> HashMap<String, Datapoint> {
     let url_re = Regex::new(r"(https://.?.img.fmi.fi/php/img.php[\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])").unwrap();
     let timestamp_re = Regex::new(r"(\d{12})").unwrap();
     // zip iterators from both regexp searches into one and loop through them simultaneously
-    let mut datapoints = HashMap::new();
+    let mut datapoints = BTreeMap::new();
     let matrix = url_re.captures_iter(&body).zip(timestamp_re.captures_iter(&body));
     for (url, timestamp) in matrix {
-        println!("{} {}", &timestamp[1], &url[1]);
+        //println!("{} {}", &timestamp[1], &url[1]);
         let datapoint = Datapoint::new(url[1].to_string(), timestamp[1].to_string()).unwrap();
         datapoints.insert(String::from(&timestamp[1]), datapoint);
     }
