f3fc4e5d7b8ed80dcb64bd75be848aef2b870d14
Antti Peltonen <antti.peltonen@iki.fi>
2018-08-25 19:15:09 +0300

    progressbar shows timestamp and position in btree

--- src/gui.rs
+++ src/gui.rs
@@ -4,7 +4,7 @@ use gtk;
 use gio::prelude::*;
 use gtk::prelude::*;
 
-use gtk::{Application, ApplicationWindow, Builder, Image};
+use gtk::{Application, ApplicationWindow, Builder, Image, ProgressBar};
 
 use std::env::args;
 use std::collections::BTreeMap;
@@ -36,7 +36,8 @@ pub struct TestbedGui {
     application: Application,
     datapoints: Rc<RefCell<BTreeMap<String, Datapoint>>>,
     datapoint_image: Rc<RefCell<Image>>,
-    datapoint_position: Rc<RefCell<usize>>
+    datapoint_position: Rc<RefCell<usize>>,
+    progress_bar: Rc<RefCell<ProgressBar>>
 }
 impl TestbedGui {
     pub fn new() -> TestbedGui {
@@ -45,8 +46,9 @@ impl TestbedGui {
             .expect("Initialization failed...");
 
         let img_datapoint: Rc<RefCell<Image>> = Rc::new(RefCell::new(Image::new()));
-
         let img_datapoint_clone = img_datapoint.clone();
+        let pbar_position: Rc<RefCell<ProgressBar>> = Rc::new(RefCell::new(ProgressBar::new()));
+        let pbar_position_clone = pbar_position.clone();
         application.connect_startup(move |app| {
             // added during compilation (macro)
             let glade_src = include_str!("fmitestbed.glade");
@@ -55,6 +57,10 @@ impl TestbedGui {
             let main_window: ApplicationWindow = builder.get_object("main_window").expect("Couldn't get main_window");
             let img_dbz: Image = builder.get_object("img_dBZ").expect("Could'nt get img_dBZ");
             *img_datapoint_clone.borrow_mut() = builder.get_object("img_datapoint").expect("Could'nt get img_datapoint");
+            *pbar_position_clone.borrow_mut() = builder.get_object("pbar_position").expect("Could'nt get pbr_position");
+            *&pbar_position_clone.borrow_mut()
+                .set_show_text(true);
+
             // loaded during runtime
             img_dbz.set_from_file("resources/dbz.png");
 
@@ -71,7 +77,8 @@ impl TestbedGui {
         TestbedGui { application: application,
             datapoints: Rc::new(RefCell::new(parse_testbed())),
             datapoint_image: img_datapoint,
-            datapoint_position: Rc::new(RefCell::new(0)) }
+            datapoint_position: Rc::new(RefCell::new(0)),
+            progress_bar: pbar_position }
     }
 
     pub fn run(&self) {
@@ -91,17 +98,19 @@ impl TestbedGui {
         let datapoints_clone = self.datapoints.clone();
         let datapoints_position_clone = self.datapoint_position.clone();
         let img_datapoint_clone = self.datapoint_image.clone();
+        let pbar_position_clone = self.progress_bar.clone();
         let tock = move || {
             //println!("tock");
             let map = &*datapoints_clone.borrow();
-
-            let mut position: usize = 0;
             let old_position = *datapoints_position_clone.borrow_mut();
+            let mut position: usize = 0;
             for (_key, val) in map {
                 if position > old_position {
                     //println!("get pixbuf for {}", _key);
-                    let pixbuf = val.image_as_pixbuf();
-                    *&img_datapoint_clone.borrow_mut().set_from_pixbuf(&pixbuf);
+                    *&img_datapoint_clone.borrow_mut()
+                        .set_from_pixbuf(&val.image_as_pixbuf());
+                    *&pbar_position_clone.borrow_mut()
+                        .set_text(val.timestamp().format("%Y-%m-%d %H:%M:%S").to_string().as_ref());
                     break;
                 }
                 position = position + 1;
@@ -110,6 +119,11 @@ impl TestbedGui {
             if position == 14 {
                 // when finding the end roll back to begin
                 position = 0;
+                // update progressbar state to show that we are at end of btree
+                *&pbar_position_clone.borrow_mut().set_fraction(1.0);
+            } else {
+                // update progressbar state to represent our position in the btree
+                *&pbar_position_clone.borrow_mut().set_fraction((position as f64 / 14.0) as f64);
             }
             *datapoints_position_clone.borrow_mut() = position;
             gtk::Continue(true)
